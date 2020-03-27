05a9c74d5d4fb27a2d9d37f1457e14f2cfbc67d2
Juan Montes <juan.montes@globant.com>
2020-03-27 11:17:11 -0500

    [ADD] Listener

---
+++ src/main/java/com/globant/web/listeners/Listener.java
@@ -0,0 +1,51 @@
+package com.globant.web.listeners;
+
+import org.apache.log4j.Logger;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+/**
+ * Class listeners.
+ * @author juan.montes
+ */
+public class Listener implements ITestListener {
+	
+	private Logger log = Logger.getLogger(Listener.class);
+
+	@Override
+	public void onFinish(ITestContext arg0) {
+
+	}
+
+	@Override
+	public void onStart(ITestContext arg0) {
+
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+
+	}
+
+	@Override
+	public void onTestFailure(ITestResult arg0) {
+		log.info("THE TEST FAILED");
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult arg0) {
+
+	}
+
+	@Override
+	public void onTestStart(ITestResult arg0) {
+
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult arg0) {
+		log.info("THE TEST PASSED");
+	}
+
+}
